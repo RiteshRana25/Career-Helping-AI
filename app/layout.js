b7f83a6f22e3c98b87eea5a53281ad1c9e3c9a50
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2025-03-19 20:53:46 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -33,11 +33,11 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             <Toaster richColors/>
             {/* {footer} */}
-            <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>thank you</p>
-              </div>
-            </footer>
+            // <footer className="bg-muted/50 py-12">
+            //   <div className="container mx-auto px-4 text-center text-gray-200">
+            //     <p>thank you</p>
+            //   </div>
+            // </footer>
           </ThemeProvider>
         </body>
       </html>
